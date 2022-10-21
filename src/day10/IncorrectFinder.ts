b427a38b390e1789c47d6545cec87a608b2e908e
Protasenya, Aleksey S <ProtasenyaAS@qulix.com>
2022-10-21 14:48:09 +0300

    day 10 part 2

--- src/day10/IncorrectFinder.ts
+++ src/day10/IncorrectFinder.ts
@@ -60,4 +60,72 @@ export class IncorrectFinder {
         }
         return 0;
     }
+
+    private setResultCharacters() {
+        for (let character of this.characterRow) {
+            switch (character) {
+                case '(':
+                case '[':
+                case '{':
+                case '<':
+                    this.elements.push(character);
+                    break;
+                case ')': {
+                    const elementsLength = this.elements.length;
+                    if (this.elements[elementsLength - 1] === '(') {
+                        this.elements.pop();
+                    }
+                    break;
+                }
+                case ']': {
+                    const elementsLength = this.elements.length;
+                    if (this.elements[elementsLength - 1] === '[') {
+                        this.elements.pop();
+                    }
+                    break;
+                }
+                case '}': {
+                    const elementsLength = this.elements.length;
+                    if (this.elements[elementsLength - 1] === '{') {
+                        this.elements.pop();
+                    }
+                    break;
+                }
+                case '>': {
+                    const elementsLength = this.elements.length;
+                    if (this.elements[elementsLength - 1] === '<') {
+                        this.elements.pop();
+                    }
+                    break;
+                }
+                default: {
+                }
+            }
+        }
+    }
+
+    findResult = () => {
+        this.setResultCharacters();
+        let result = 0;
+        this.elements.reverse().forEach((character) => {
+            result *= 5;
+            switch (character) {
+                case '(':
+                    result += Points[')'];
+                    break;
+                case '[':
+                    result += Points[']'];
+                    break;
+                case '{':
+                    result += Points['}'];
+                    break;
+                case '<':
+                    result += Points['>'];
+                    break;
+                default:
+                    break;
+            }
+        });
+        return result;
+    };
 }
--- src/day10/index.ts
+++ src/day10/index.ts
@@ -1,9 +1,19 @@
 import { splitToCharacters } from '../utils/retrieveData';
 import { IncorrectFinder } from './IncorrectFinder';
 
-export const getResult = () => {
+const correctRows: string[][] = [];
+
+const getCorrectRows = () => {
     const retrievedData = splitToCharacters();
-    return retrievedData.reduce((acc, curr) => {
-        return acc + new IncorrectFinder(curr).findIncorrect();
-    }, 0);
+    return retrievedData.filter(
+        (curr) => !new IncorrectFinder(curr).findIncorrect(),
+    );
+};
+
+export const getResult = () => {
+    const correctRows = getCorrectRows();
+    const results = correctRows
+        .map((row) => new IncorrectFinder(row).findResult())
+        .sort((a, b) => a - b);
+    return results[Math.floor(results.length / 2)];
 };
