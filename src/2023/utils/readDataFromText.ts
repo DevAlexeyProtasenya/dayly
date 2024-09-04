02f3869c485a1bc7c815ab2f7ac9e3d7c47ad4dc
DevAlexeyProtasenya <37490289+DevAlexeyProtasenya@users.noreply.github.com>
2024-09-04 12:06:59 +0300

    2023|day3|part1

--- src/2023/utils/readDataFromText.ts
+++ src/2023/utils/readDataFromText.ts
@@ -2,9 +2,9 @@ import fs from 'fs';
 import path from 'path';
 
 export const retrieveData = (relativePath: string): string => {
-  const data = fs.readFileSync(path.resolve(relativePath));
-  return data.toString();
+    const data = fs.readFileSync(path.resolve(relativePath));
+    return data.toString();
 };
 
 export const parseToArray = (data: string): Array<string> =>
-  data.replace(/(\r\n|\r|\n)/gm, '/').split('/');
+    data.replace(/(\r\n|\r|\n)/gm, ',').split(',');
